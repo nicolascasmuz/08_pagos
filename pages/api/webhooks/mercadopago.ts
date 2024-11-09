b0ab816cecd414523dffa711b94981592cccb935
nicolascasmuz <nicolascasmuz@hotmail.com>
2024-11-09 00:15:40 -0300

    commit-002

--- pages/api/webhooks/mercadopago.ts
+++ pages/api/webhooks/mercadopago.ts
@@ -1,17 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
 import { getMerchantOrder } from "lib/mercadopago";
-
-/* export default methods({
-  async get(req: NextApiRequest, res: NextApiResponse) {
-    const { id, topic } = req.query;
-    if (topic == "merchant_order") {
-      await getMerchantOrder(id);
-    }
-
-    res.send("ok");
-  },
-}); */
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { id, topic } = req.query;
